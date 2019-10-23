87cd0e074b3eb8deadbd97bd4fb89677f4a9dbb1
syz <eagle-china@hotmail.com>
2019-10-23 20:24:05 +0800

    support new version yaml

--- serverless.js
+++ serverless.js
@@ -19,62 +19,36 @@ const {
   DeleteUsagePlan,
   DeleteApiKey,
   UnReleaseService,
-  DisableApiKey
+  DisableApiKey,
+  Validate,
+  DescribeUsagePlan
 } = require('./utils')
 
-const defaults = {
-  Region: 'ap-guangzhou',
-  protocol: 'http',
-  maxRequestNumPreSec: 1000,
-  maxRequestNum: -1,
-  authRequired: 'TRUE', // 是否需要签名认证，TRUE表示需要，FALSE 表示不需要。默认为 TRUE。如果需要开放在云市场的 API，必须选择 TRUE。
-  enableCORS: 'TRUE',
-  serviceScfFunctionQualifier: '$LATEST',
-  serviceScfIsIntegratedResponse: 'FALSE',
-  serviceTimeout: 15,
-  environment: 'release'
-}
+const serviceType    = 'SCF'
+const serviceTimeout = 15
+const bindType       = 'API'
 
 class TencentApiGateway extends Component {
   async default(inputs = {}) {
     this.context.status('Deploying')
-    const config = {
-      ...defaults,
-      ...inputs,
-      serviceType: 'SCF',
-      apiType: 'NORMAL',
-      bindType: 'API'
-    }
-    config.apiName = this.id.split('Template.')[1]
+    
+    inputs.apiName = this.id.split('Template.')[1]
+
+    const params = Validate(inputs);
     let {
-      Region,
-      apiId,
+      region,
       serviceId,
-      serviceDesc,
+      description,
       serviceName,
       apiName,
-      apiDesc,
-      apiType,
-      authRequired,
-      enableCORS,
-      requestConfig,
-      serviceType,
-      serviceTimeout,
-      serviceScf,
       protocol,
-      usagePlanId,
-      usagePlanName,
-      usagePlanDesc,
-      maxRequestNumPreSec,
-      maxRequestNum,
-      secretIds,
-      secretName,
+      usagePlan,
       environment,
-      bindType
-    } = config
+      endpoints
+    } = params
 
     this.context.debug(
-      `Starting API Gateway deployment with name ${apiName} in the ${Region} region`
+      `Starting API Gateway deployment with name ${apiName} in the ${region} region`
     )
 
     const apig = new Capi({
@@ -82,207 +56,234 @@ class TencentApiGateway extends Component {
       SecretKey: this.context.credentials.tencent.SecretKey,
       serviceType: 'apigateway'
     })
-    this.state.Region = Region
+    this.state.region = region
     await this.save()
 
-    const serviceInputs = { Region, serviceDesc, serviceName, protocol: protocol.toLowerCase() }
+    const serviceInputs = { 
+      Region: region, 
+      serviceDesc: description, 
+      // Up to 50 characters，(a-z,A-Z,0-9,_)
+      serviceName, 
+      protocol: protocol.toLowerCase() 
+    }
+
+    let subDomain = '';
     if (!serviceId) {
       this.context.debug(`Service ID not found in state. Creating a new Service.`)
       const serviceMsg = await CreateService({ apig, ...serviceInputs })
       this.context.debug(`Service with ID ${serviceMsg.serviceId} created.`)
       serviceId = serviceMsg.serviceId
-      console.log(serviceMsg)
-      this.state.serviceId = serviceMsg.serviceId
-      this.state.subDomain = serviceMsg.subDomain
-      await this.save()
+      subDomain = serviceMsg.subDomain
     } else {
       this.context.debug(`Updating Service with serviceId ${serviceId}.`)
       await ModifyService({ apig, serviceId, ...serviceInputs })
-      const serviceMsg = await DescribeService({ apig, Region, serviceId })
+      const serviceMsg = await DescribeService({ apig, Region: region, serviceId })
       this.context.debug(`Service with ID ${serviceId} Updated.`)
-      this.state.serviceId = serviceMsg.serviceId
-      this.state.subDomain = serviceMsg.subDomain
-      await this.save()
+      subDomain = serviceMsg.subDomain
     }
 
-    const apiInputs = {
-      Region,
-      serviceId,
-      apiName,
-      apiDesc,
-      apiType,
-      authRequired: authRequired.toString().toLocaleUpperCase(),
-      enableCORS: enableCORS.toString().toLocaleUpperCase(),
-      requestConfig,
-      serviceType,
-      serviceTimeout,
-      serviceScfFunctionName: serviceScf.FunctionName,
-      serviceScfIsIntegratedResponse: serviceScf.IsIntegratedResponse.toString().toLocaleUpperCase(),
-      serviceScfFunctionQualifier: serviceScf.FunctionQualifier.toString().toLocaleUpperCase()
-    }
+    const apiAuthSetting = async (ctx, region, apiClient, endpoint) => {
 
-    if (!apiId) {
-      this.context.debug(`API ID not found in state. Creating a new API.`)
-      apiId = await CreateApi({ apig, ...apiInputs })
-      this.context.debug(`API with ID ${apiId} created.`)
-      this.state.apiId = apiId
-      await this.save()
-    } else {
-      this.context.debug(`Updating api with apiId ${apiId}.`)
-      apiId = await ModifyApi({ apig, apiId, ...apiInputs })
-      this.context.debug(`Service with ID ${apiId} Updated.`)
-      this.state.apiId = apiId
-      await this.save()
-    }
-
-    if (authRequired) {
+      const usagePlan = endpoint.usagePlan
       const usageInputs = {
-        Region,
-        usagePlanName: usagePlanName || `usagePlanName_${apiId.split('api-')[1]}`,
-        usagePlanDesc,
-        maxRequestNumPreSec,
-        maxRequestNum
-      }
-      if (!usagePlanId) {
-        this.context.debug(`UsagePlan ID not found in state. Creating a new UsagePlan.`)
-        usagePlanId = await CreateUsagePlan({ apig, ...usageInputs })
-        this.context.debug(`UsagePlan with ID ${usagePlanId} created.`)
-        this.state.usagePlanId = usagePlanId
-        await this.save()
-      } else {
-        this.context.debug(`Updating UsagePlan with usagePlanId ${usagePlanId}.`)
-        usagePlanId = await ModifyUsagePlan({ apig, usagePlanId, ...usageInputs })
-        this.context.debug(`UsagePlan with ID ${usagePlanId} Updated.`)
-        this.state.usagePlanId = usagePlanId
-        await this.save()
+        Region: region,
+        usagePlanName: usagePlan.usagePlanName || `usagePlanName_${endpoint.apiId.split('api-')[1]}`,
+        usagePlanDesc: usagePlan.usagePlanDesc || '',
+        maxRequestNumPreSec: usagePlan.maxRequestNumPreSec,
+        maxRequestNum: usagePlan.maxRequestNum
       }
 
-      if (!secretIds) {
-        this.context.debug(`Secretkey List not found in state. Creating a new Secretkey.`)
-        const SecretMsg = await CreateApiKey({
-          apig,
-          Region,
-          secretName: secretName || `secretName_${apiId.split('api-')[1]}`,
+      let usagePlanId = null;
+      if (!usagePlan.usagePlanId) {
+        ctx.context.debug(`UsagePlan ID not found in state. Creating a new UsagePlan.`)
+        usagePlanId = await CreateUsagePlan({ apig: apiClient, ...usageInputs })
+        ctx.context.debug(`UsagePlan with ID ${usagePlanId} created.`)
+      } else {
+        await DescribeUsagePlan({apig: apiClient, usagePlanId, Region: region})
+
+        ctx.context.debug(`Updating UsagePlan with usagePlanId ${usagePlanId}.`)
+        usagePlanId = await ModifyUsagePlan({ apig: apiClient, usagePlanId, ...usageInputs })
+        ctx.context.debug(`UsagePlan with ID ${usagePlanId} Updated.`)
+      }
+
+      let secretIds = endpoint.auth.secretIds;
+      if (!endpoint.auth.secretIds) {
+        ctx.context.debug(`Secretkey List not found in state. Creating a new Secretkey.`)
+        const secretMsg = await CreateApiKey({
+          apig: apiClient,
+          Region: region,
+          secretName: endpoint.auth.secretName || `secretName_${endpoint.apiId.split('api-')[1]}`,
           type: 'auto'
         })
-        this.context.debug(
-          `Secretkey with ID ${SecretMsg.secretId} and KEY ${SecretMsg.secretKey} Updated.`
+        ctx.context.debug(
+          `Secretkey with ID ${secretMsg.secretId} and KEY ${secretMsg.secretKey} Updated.`
         )
-        this.state.secretId = SecretMsg.secretId
-        secretIds = [SecretMsg.secretId]
-        this.state.secretIds = secretIds
-        await this.save()
+        ctx.state.secretId = secretMsg.secretId
+        secretIds = [secretMsg.secretId]
+        ctx.state.secretIds = secretIds
+        await ctx.save()
       }
-      this.context.debug(`Binding Secretkey to UsagePlan with ID ${usagePlanId}.`)
-      this.state.secretIds = secretIds
-      this.state.environment = environment
-      await this.save()
-      await BindSecretIds({ apig, Region, secretIds, usagePlanId })
-      this.context.debug(`Binding sucessed.`)
-      this.context.debug(
-        `Binding UsagePlan with ID ${usagePlanId} to Service with ID ${serviceId}.`
-      )
-      await BindEnvironment({
-        apig,
-        Region,
-        usagePlanIds: [usagePlanId],
-        serviceId,
-        environment,
-        bindType,
-        apiIds: [apiId]
-      })
-      this.context.debug(`Binding sucessed.`)
-    }
-    this.context.debug(`Deploying service with ID ${serviceId}.`)
-    await ReleaseService({
-      apig,
-      Region,
-      serviceId,
-      environmentName: environment,
-      releaseDesc: '由Serverless Api-gateway Component自动发布'
-    })
-    this.context.debug(
-      `Deployment successful for the API named ${apiName} in the ${Region} region.`
-    )
-    const outputs = {
-      protocol,
-      serviceId,
-      subDomain: this.state.subDomain,
-      requestConfig
-    }
-    if (authRequired) {
-      outputs.identity = { secretIds: secretIds || [this.state.secretId] }
-    }
-    this.context.debug(`${JSON.stringify(outputs)}`)
 
-    return outputs
+      return {
+        usagePlanId,
+        secretIds
+      }
+
+    }
+
+    const outputs = [];
+    const len = endpoints.length;
+    for (let i = 0; i < len; i++) {
+      const endpoint = endpoints[i];
+
+      const requestConfig = {
+        path: endpoint.path,
+        method: endpoint.method
+      }
+
+      const output = {
+        protocol,
+        serviceId,
+        subDomain,
+        environment,
+        requestConfig
+      }
+
+      const apiInputs = {
+        Region: region,
+        serviceId,
+        apiName,
+        apiDesc: endpoint.description,
+        apiType: 'NORMAL',
+        authRequired: endpoint.auth ? 'TRUE' : 'FALSE',
+        enableCORS: endpoint.enableCORS ? 'TRUE' : 'FALSE',
+        requestConfig,
+        serviceType,
+        serviceTimeout,
+        serviceScfFunctionName: endpoint.function.functionName,
+        serviceScfIsIntegratedResponse: endpoint.function.isIntegratedResponse ? 'TRUE' : 'FALSE',
+        serviceScfFunctionQualifier: endpoint.function.functionQualifier ? 'TRUE' : 'FALSE'
+      }
+      let apiId = null;
+      if (!endpoint.apiId) {
+        this.context.debug(`API ID not found in state. Creating a new API.`)
+        apiId = await CreateApi({ apig, ...apiInputs })
+        this.context.debug(`API with ID ${apiId} created.`)
+      } else {
+        this.context.debug(`Updating api with apiId ${apiId}.`)
+        apiId = await ModifyApi({ apig, apiId, ...apiInputs })
+        this.context.debug(`Service with ID ${apiId} Updated.`)
+      }
+      output.apiId = apiId;
+
+      let secretIds;
+      if (endpoint.auth) {
+        const result = await apiAuthSetting(this, region, apig, endpoint)
+        this.context.debug(`Binding Secretkey to UsagePlan with ID ${result.usagePlanId}.`)
+
+        await BindSecretIds({ apig, Region: region, 
+                          secretIds: result.secretIds, 
+                          usagePlanId: result.usagePlanId })
+        this.context.debug(`Binding sucessed.`)
+        this.context.debug(
+          `Binding UsagePlan with ID ${result.usagePlanId} to Api with path ${endpoint.method}:${endpoint.path}.`
+        )
+
+        await BindEnvironment({
+          apig,
+          Region: region,
+          usagePlanIds: [result.usagePlanId],
+          serviceId,
+          environment, 
+          bindType,
+          apiIds: [apiId]
+        })
+        secretIds = result.secretIds
+        this.context.debug(`Binding sucessed.`)
+        output.usagePlanId = result.usagePlanId
+      }
+      output.secretIds = secretIds
+
+      this.context.debug(`Deploying service with ID ${serviceId}.`)
+      await ReleaseService({
+        apig,
+        Region: region,
+        serviceId,
+        environmentName: environment,
+        releaseDesc: 'Serverless Api-gateway Component Deploy'
+      })
+      this.context.debug(
+        `Deployment successful for the API named ${apiName} in the ${region} region.`
+      )
+      
+      outputs.push(output)
+    }
+    
+    this.state.apis = outputs
+    await this.save()
+
+    return outputs;
   }
 
   async remove(inputs = {}) {
-    const { Region, apiId, serviceId, secretIds, usagePlanId, apiName, environment } = this.state
-    this.context.status('Removing')
-    this.context.debug(
-      `Starting API Gateway deployment with name ${apiName} in the ${Region} region`
-    )
 
+    if (!this.state.apis) {
+      this.context.debug(`Aborting removal. Function name not found in state.`)
+      return
+    }
     const apig = new Capi({
-      SecretId: this.context.credentials.tencent.tencent_secret_id,
-      SecretKey: this.context.credentials.tencent.tencent_secret_key,
+      SecretId: this.context.credentials.tencent.SecretId,
+      SecretKey: this.context.credentials.tencent.SecretKey,
       serviceType: 'apigateway'
     })
 
-    if (this.state.apiId) {
-      this.context.debug(
-        `API ID ${this.state.id} found in state. Removing from the ${this.state.Region}.`
-      )
-      await UnBindSecretIds({ apig, Region, secretIds, usagePlanId })
-      this.context.debug(`Unbinding Secretkey to UsagePlan with ID ${usagePlanId}.`)
-      await UnBindEnvironment({
-        apig,
-        Region,
-        serviceId,
-        usagePlanIds: [usagePlanId],
-        environment,
-        bindType: 'API',
-        apiIds: [apiId]
-      })
-      this.context.debug(
-        `Unbinding UsagePlan with ID ${usagePlanId} to Service with ID ${serviceId}.`
-      )
-      await DeleteUsagePlan({ apig, Region, usagePlanId })
+    const region = this.state.region;
+    const apisLen = this.state.apis.length;
+    for (let i = 0; i < apisLen; i++) {
+      const endpoint = this.state.apis[i]
+      if (!endpoint.apiId) continue
+
+      if (endpoint.usagePlanId) {
+        await UnBindSecretIds({ apig, Region: region, 
+                    secretIds: endpoint.secretIds, usagePlanId: endpoint.usagePlanId })
+        this.context.debug(`Unbinding Secretkey to UsagePlan with ID ${endpoint.usagePlanId}.`)
+        await UnBindEnvironment({
+          apig,
+          Region: region,
+          serviceId: endpoint.serviceId,
+          usagePlanIds: [endpoint.usagePlanId],
+          environment: endpoint.environment,
+          bindType,
+          apiIds: [endpoint.apiId]
+        })
+        this.context.debug(
+          `Unbinding UsagePlan with ID ${endpoint.usagePlanId} to Service with ID ${endpoint.serviceId}.`
+        )
+        await DeleteUsagePlan({ apig, Region: region, usagePlanId: endpoint.usagePlanId })
+      }
+
       this.context.debug(`Removing any previously deployed usagePlanIds.`)
-      if (secretIds instanceof Array && secretIds.length > 0) {
-        secretIds.map(async (secretId) => {
-          await DisableApiKey({ apig, Region, secretId })
-          await DeleteApiKey({ apig, Region, secretId })
+      if (endpoint.secretIds instanceof Array && endpoint.secretIds.length > 0) {
+        endpoint.secretIds.map(async (secretId) => {
+          await DisableApiKey({ apig, Region: region, secretId })
+          await DeleteApiKey({ apig, Region: region, secretId })
         })
       }
       this.context.debug(`Removing any previously deployed SecretKey.`)
-      await DeleteApi({ apig, Region, apiId, serviceId })
+      await DeleteApi({ apig, Region: region, 
+                apiId: endpoint.apiId, serviceId: endpoint.serviceId})
       this.context.debug(`Removing any previously deployed API.`)
       await UnReleaseService({
         apig,
-        Region,
-        serviceId,
-        environmentName: environment,
-        unReleaseDesc: '由Serverless Api-gateway Component自动下线'
+        Region: region,
+        serviceId: endpoint.serviceId,
+        environmentName: endpoint.environment,
+        unReleaseDesc: 'Serverless Api-gateway Component Offline'
       })
-    } else {
-      this.context.debug(`No API ID found in state.`)
     }
-    const outputs = {
-      Region,
-      apiId,
-      serviceId,
-      secretIds,
-      usagePlanId,
-      environment
-    }
-
-    this.context.debug(`Flushing state for the API Gateway component.`)
+    const outputs = this.state.apis
     this.state = {}
     await this.save()
-    this.context.debug(`${JSON.stringify(outputs)}`)
     return outputs
   }
 }
