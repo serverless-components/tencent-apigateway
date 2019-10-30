00904b6f76ab2420218e7bf46b19511b795c43db
syz <eagle-china@hotmail.com>
2019-10-30 16:21:19 +0800

    fixed yaml check error message inaccurate

--- utils.js
+++ utils.js
@@ -515,6 +515,28 @@ const Validate = (config) => {
       .error(new Error('"usagePlan.usagePlanName" is required'))
   }
 
+  // Api returns a maximum of 100 rows of records at a time
+  const endpointsScheme = Joi.array().max(100).items(Joi.object().keys({
+    apiId: Joi.string().optional(),
+    description: Joi.string().max(200).optional(),
+    enableCORS: Joi.boolean().optional().default(true),
+    path: Joi.string().required(),
+    method: Joi.string().regex(/^(GET|POST|PUT|DELETE|HEAD|ANY)$/).required(),
+    function: Joi.object().keys({
+      isIntegratedResponse: Joi.boolean().optional().default(true),
+      functionQualifier: Joi.string().optional().default('$LATEST'),
+      functionName: Joi.string().required().error(new Error('"endpoints.function.functionName" is required'))
+    }).required(),
+    usagePlan: Joi.object().keys(usagePlanScheme),
+    auth: {
+      serviceTimeout: Joi.number().integer().optional().default(15),
+      secretName: Joi.string().required(),
+      // Api returns a maximum of 100 rows of records at a time
+      // https://cloud.tencent.com/document/product/628/14920
+      secretIds: Joi.array().max(100)
+    }
+  })).required()
+
   const globalScheme = Joi.object()
     .keys({
       region: Joi.string()
@@ -536,7 +558,8 @@ const Validate = (config) => {
       environment: Joi.string()
         .regex(/^(prepub|test|release)$/)
         .optional()
-        .default('release')
+        .default('release'),
+      endpoints: endpointsScheme
       // usagePlan: Joi.object().keys(usagePlanScheme)
     })
     .options({ allowUnknown: true })
@@ -544,33 +567,6 @@ const Validate = (config) => {
   const gloalResult = Joi.validate(config, globalScheme)
   if (gloalResult.error) {
     throw gloalResult.error
-  }
-
-  // Api returns a maximum of 100 rows of records at a time
-  const endpointsScheme = Joi.array().max(100).items(Joi.object().keys({
-    apiId: Joi.string().optional(),
-    description: Joi.string().max(200).optional(),
-    enableCORS: Joi.boolean().optional().default(true),
-    path: Joi.string().required(),
-    method: Joi.string().regex(/^(GET|POST|PUT|DELETE|HEAD|ANY)$/).required(),
-    function: Joi.object().keys({
-      isIntegratedResponse: Joi.boolean().optional().default(true),
-      functionQualifier: Joi.string().optional().default('$LATEST'),
-      functionName: Joi.string().required().error(new Error('"endpoints.function.functionName" is required'))
-    }).required(),
-    usagePlan: Joi.object().keys(usagePlanScheme),
-    auth: {
-      serviceTimeout: Joi.number().integer().optional().default(15),
-      secretName: Joi.string().required(),
-      // Api returns a maximum of 100 rows of records at a time
-      // https://cloud.tencent.com/document/product/628/14920
-      secretIds: Joi.array().max(100)
-    }
-  })).required().error(new Error('endpoints must contain less than or equal to 100 items'));
-
-  const endpointsResult = Joi.validate(config.endpoints, endpointsScheme)
-  if (endpointsResult.error) {
-    throw endpointsResult.error
   }
 
   return gloalResult.value
